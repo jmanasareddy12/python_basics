35d806e30c99248a6feef9bd7468284be647a30c
jmanasareddy12 <jmanasareddy12@gmail.com>
2026-06-25 16:53:58 +0530

    python_more

--- test.py
+++ test.py
@@ -2,4 +2,18 @@
 # print("Hello World!", "world", sep="#")
 # print("a">"b")
 x="10.5"
-print(x)
+print(int(float(x)))
+print("\test\ntesting")
+print(r"\test\ntesting")
+print("\\test\\ntesting")
+print("""hello
+      world""")
+x="hello"
+print(x[0:3:2])
+print(x[::-1])
+for i in range(1,10):
+    if i%2==0:
+
+for i in range(0,10,2):
+    if i%2==0:
+        print(i)
